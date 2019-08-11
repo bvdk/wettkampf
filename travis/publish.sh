1dd42af8f40f0bffc32cbf1abbacc918ef9b920d
Marcel Rösler <roesler1@gmx.de>
2019-08-11 21:01:33 +0200

    support multiple architectures part 8

--- travis/publish.sh
+++ travis/publish.sh
@@ -4,6 +4,12 @@ VERSION="$(npx -c 'echo "$npm_package_version"')"
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin;
 
+if [ "$ARCH" == "amd64" ]; then
+    docker push "bvdk2019/entry:latest";
+    docker push "bvdk2019/backend:latest";
+    docker push "bvdk2019/frontend:latest";
+fi
+
 docker push "bvdk2019/entry:$ARCH-latest";
 docker push "bvdk2019/backend:$ARCH-latest";
 docker push "bvdk2019/frontend:$ARCH-latest";
