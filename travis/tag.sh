db8ab441e4f3fe18238b58dbe40dd195a1a8e97f
Marcel Rösler <roesler1@gmx.de>
2019-08-11 20:25:10 +0200

    support multiple architectures part 4

--- travis/tag.sh
+++ travis/tag.sh
@@ -2,6 +2,6 @@
 
 VERSION="$(npx -c 'echo "$npm_package_version"')"
 
-docker tag bvdk2019/entry bvdk2019/entry:$VERSION;
-docker tag bvdk2019/backend bvdk2019/backend:$VERSION;
-docker tag bvdk2019/frontend bvdk2019/frontend:$VERSION;
+docker tag bvdk2019/entry "bvdk2019/entry:$ARCH-$VERSION";
+docker tag bvdk2019/backend "bvdk2019/backend:$ARCH-$VERSION";
+docker tag bvdk2019/frontend "bvdk2019/frontend:$ARCH-$VERSION";
