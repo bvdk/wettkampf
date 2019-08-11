1e34f3c84cf8e299aecaab818bee2eb698b403c4
Marcel Rösler <roesler1@gmx.de>
2019-08-11 20:47:50 +0200

    support multiple architectures part 7

--- travis/publish.sh
+++ travis/publish.sh
@@ -3,9 +3,11 @@
 VERSION="$(npx -c 'echo "$npm_package_version"')"
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin;
-docker push bvdk2019/entry:latest;
-docker push bvdk2019/entry:$VERSION;
-docker push bvdk2019/backend:latest;
-docker push bvdk2019/backend:$VERSION;
-docker push bvdk2019/frontend:latest;
-docker push bvdk2019/frontend:$VERSION;
+
+docker push "bvdk2019/entry:$ARCH-latest";
+docker push "bvdk2019/backend:$ARCH-latest";
+docker push "bvdk2019/frontend:$ARCH-latest";
+
+docker push "bvdk2019/entry:$ARCH-$VERSION";
+docker push "bvdk2019/backend:$ARCH-$VERSION";
+docker push "bvdk2019/frontend:$ARCH-$VERSION";
--- travis/tag.sh
+++ travis/tag.sh
@@ -2,6 +2,10 @@
 
 VERSION="$(npx -c 'echo "$npm_package_version"')"
 
+docker tag bvdk2019/entry "bvdk2019/entry:$ARCH-latest";
+docker tag bvdk2019/backend "bvdk2019/backend:$ARCH-latest";
+docker tag bvdk2019/frontend "bvdk2019/frontend:$ARCH-latest";
+
 docker tag bvdk2019/entry "bvdk2019/entry:$ARCH-$VERSION";
 docker tag bvdk2019/backend "bvdk2019/backend:$ARCH-$VERSION";
 docker tag bvdk2019/frontend "bvdk2019/frontend:$ARCH-$VERSION";
