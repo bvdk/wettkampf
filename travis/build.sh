d766b4302f67056a8fa7c412f99fb5947d8754c5
Marcel Rösler <roesler1@gmx.de>
2019-08-11 19:36:16 +0200

    final docker-compose-production file, build multi arch docker images

--- travis/build.sh
+++ travis/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+docker run --rm --privileged multiarch/qemu-user-static:register --reset
 docker build -f docker/production/entry/Dockerfile -t bvdk2019/entry .;
 docker build -f docker/production/backend/Dockerfile -t bvdk2019/backend .;
 docker build -f docker/production/frontend/Dockerfile -t bvdk2019/frontend .;
