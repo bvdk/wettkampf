870c0419c4e8f86c9e818eb96b80f5102428c369
Marcel Rösler <roesler1@gmx.de>
2019-08-11 18:18:37 +0200

    try getting travis to build images part 9

--- travis/publish.sh
+++ travis/publish.sh
@@ -2,10 +2,6 @@
 
 VERSION="$(npx -c 'echo "$npm_package_version"')"
 
-echo "$VERSION";
-echo "$DOCKER_PASSWORD";
-echo "$DOCKER_USERNAME";
-
 docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD" --password-stdin;
 docker push bvdk2019/entry:latest;
 docker push bvdk2019/backend:latest;
