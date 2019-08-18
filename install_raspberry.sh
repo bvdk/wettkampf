fe205644221822201376e494d5d66460ff2e01ff
Marcel Rösler <roesler1@gmx.de>
2019-08-18 08:58:53 +0200

    add raspberry pi install script

---
+++ install_raspberry.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+echo "start install dependencies";
+sudo apt-get update && sudo apt-get dist-upgrade;
+sudo apt-get -y install apt-transport-https ca-certificates software-properties-common;
+
+echo "start install docker";
+curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh;
+sudo usermod -aG docker pi;
+sudo curl https://download.docker.com/linux/raspbian/gpg;
+curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add -;
+sudo add-apt-repository "deb https://download.docker.com/linux/raspbian/ $(lsb_release -cs) stable";
+
+echo "run docker";
+sudo docker run hello-world;
