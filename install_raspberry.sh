c43ca926e8d92e33ac1e56657bc3f1dd6d72b6af
Marcel Rösler <roesler1@gmx.de>
2019-08-24 10:37:52 +0200

    raspberry pi install script part 10

--- install_raspberry.sh
+++ install_raspberry.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
 echo "The following script installs all necessary dependencies for https://github.com/bvdk/wettkampf";
-echo "";
+echo "The installation can take a while.";
 echo "";
 echo "The raspberry pi will restart after the installation";
+echo "";
 read -p "Press enter to continue"
 
 echo "start install dependencies";
@@ -11,13 +12,16 @@ sudo apt-get update && sudo apt-get -y dist-upgrade;
 sudo apt-get -y install apt-transport-https ca-certificates software-properties-common;
 
 echo "start install docker";
-newgrp docker
-sudo groupadd docker
-sudo usermod -aG docker $USER;
-curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh;
+if ! [ -x "$(command -v docker)" ]; then
 
-sudo apt-get -y install docker-compose;
+  newgrp docker
+  sudo groupadd docker
+  sudo usermod -aG docker $USER;
+  curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh;
 
-echo "{ \"storage-driver\": \"devicemapper\" }" | sudo tee /etc/docker/daemon.json
+  sudo apt-get -y install docker-compose;
+  echo "{ \"storage-driver\": \"devicemapper\" }" | sudo tee /etc/docker/daemon.json
+
+fi
 
 sudo reboot now;
