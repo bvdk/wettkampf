e8c1ad1c99d26edb2210149c8d90b769365bc08f
marcel <roesler1@gmx.de>
2019-09-03 21:11:59 +0200

    better style

--- frontend/src/components/Flex/index.js
+++ frontend/src/components/Flex/index.js
@@ -1,39 +1,50 @@
 // @flow
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import classNames from 'classnames';
-
 
 type Props = {
     children?: any,
     className?: any,
 };
 
-
 export class Row extends Component<Props> {
-    componentDidMount() {}
-
     render() {
-        const { children, style } = this.props;
+        const {children, style} = this.props;
 
-        return <div style={style} className={classNames('flex-row',this.props.className)}>{children}</div>;
+        return <div
+            style={style}
+            className={classNames('flex-row', this.props.className)}
+        >
+            {children}
+        </div>;
     }
 }
 
 export class Col extends Component<Props> {
-
     render() {
-        const { children, style } = this.props;
+        const {children, style} = this.props;
 
-        return <div style={style} className={classNames('flex-col',this.props.className)}>{children}</div>;
+        return <div
+            style={style}
+            className={classNames('flex-col', this.props.className)}
+        >
+            {children}
+        </div>;
     }
-
 }
 
 
-export class Grow extends Component<Props>{
+export class Grow extends Component<Props> {
     render() {
-        const { children, style } = this.props;
+        const {children, style} = this.props;
 
-        return <div style={style} className={classNames('flex-grow-1',this.props.className)}>{children}</div>;
+        return (
+            <div
+                style={style}
+                className={classNames('flex-grow-1', 'grow-size-overflow', this.props.className)}
+            >
+                {children}
+            </div>
+        );
     }
 }
