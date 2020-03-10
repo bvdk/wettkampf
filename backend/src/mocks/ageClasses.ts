fd245e636f37b113eae7e946b4114e9278f9f1a7
marcel <roesler1@gmx.de>
2020-03-10 16:44:30 +0100

    fix mocks

--- backend/src/mocks/ageClasses.ts
+++ backend/src/mocks/ageClasses.ts
@@ -3,27 +3,33 @@ import { AgeClass } from "../graphql/models/ageClass";
 const ageClassesMock: AgeClass[] = [
   {
     id: "ak1",
-    name: "AK 1"
+    name: "AK 1",
+    sortId: 1001
   },
   {
     id: "ak2",
-    name: "AK 2"
+    name: "AK 2",
+    sortId: 2000
   },
   {
     id: "ak3",
-    name: "AK 3"
+    name: "AK 3",
+    sortId: 3000
   },
   {
     id: "aktive",
-    name: "Aktive"
+    name: "Aktive",
+    sortId: 1000
   },
   {
     id: "ak",
-    name: "außer Konkurrenz"
+    name: "außer Konkurrenz",
+    sortId: 9000
   },
   {
     id: "junioren",
-    name: "Junioren"
+    name: "Junioren",
+    sortId: 970
   }
 ];
 
