271eb5222519b8163ce119c93d60bc3632b43c06
marcel <roesler1@gmx.de>
2020-04-20 07:24:30 +0200

    add playground

--- backend/api/app.ts
+++ backend/api/app.ts
@@ -9,16 +9,35 @@ const run = () => {
 
     createSchema()
 
+    const path = '/api/graphql'
     app.settings.change({
       server: {
+        path,
         startMessage: (data) => console.log(data),
         playground: true,
-        path: '/api/graphql',
       },
       schema: {
         generateGraphQLSDLFile: './schema.graphql',
       },
     })
+
+    if (app.settings.current.server.playground) {
+      let playgroundHandler: (req: any, res: any) => void | undefined
+      function getPlaygroundHandler() {
+        if (playgroundHandler) return playgroundHandler
+
+        playgroundHandler = app.server.express._router.stack.find(
+          (e: any) => e.route?.path === '/' && e.route?.methods.get,
+        )?.handle
+        return playgroundHandler
+      }
+
+      app.server.express.get(`${path}/playground`, (req, res, next) => {
+        const handler = getPlaygroundHandler()
+        if (handler) return handler(req, res)
+        next() // handler is not instantly available, next() if not found
+      })
+    }
 
     app.server.start()
   } catch (e) {
